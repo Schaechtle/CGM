170271fa5502ed57e9baf0477769c913918f43ea
Schaechtle <ulrich.schaechtle@cs.rhul.ac.uk>
2015-07-19 22:51:08 -0700

    proper y limit

--- cgm_gp_samples.py
+++ cgm_gp_samples.py
@@ -185,6 +185,7 @@ def run_cgm_experiment(patient_number):
     pl.yticks(fontsize=font_size)
     ax = pl.gca()
     ax.set_xlim([0, X.shape[0]+5])
+    ax.set_ylim([np.min(X),np.max(X)])
     ticks = ax.get_xticks()
     time_stamps[0]=''
     ax.set_xticklabels([time_stamps[int(i)] for i in ticks[:-1]])
